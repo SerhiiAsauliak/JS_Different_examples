303d27c3fcc3b829e92aeb9b9b1f0b7c88f07957
Serhii <sergiomakafon@gmail.com>
2022-06-20 15:50:24 +0300

    Simple example to finding the value and area of cube

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
-"use strict";  
-// Simple tree - one side
-
-// let star = '*';
-// const size = 10;
-// while(star.length < size){
-//    console.log(star);
-//    star += '*';
-// }
-
-// Beautiful tree 
-
-const size = 10;
-
-for (let i = 0; i < size; i++){
-   let stars = '*';
-   for (let j = 0; j < i; j++){
-      stars += '**';
+"use strict";
+// Find the value and area of cube
+function calculateVolumeAndArea(side) {
+   if (typeof(side) === 'number' && side > 0 && Number.isInteger(side)) {
+      let area = 6 * Math.pow(side, 2);
+      let volume = Math.pow(side, 3);
+      return (`Обьем куба: ${volume},площадь всей поверхности: ${area}`);
+   } else {
+      return ('При вычислении произошла ошибка');
    }
-   let spacesBefore = ' '.repeat(size-i-1);
-   console.log(spacesBefore + stars);
 }
-
-// Beautiful tree - short version
-
-// for (let i = 0; i < size; i++) {
-//    let stars = '*'.repeat(2 * i + 1);
-//    let spacesBefore = ' '.repeat(size - i - 1);
-//    console.log(spacesBefore + stars);
-//  }
+console.log(calculateVolumeAndArea(5));
+console.log(calculateVolumeAndArea(15));
+console.log(calculateVolumeAndArea(15.5));
+console.log(calculateVolumeAndArea('15'));
+console.log(calculateVolumeAndArea(-15));
 
 
