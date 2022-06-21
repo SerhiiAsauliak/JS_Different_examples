e9312531ee2db006e3eb7c846892bf8d42414a56
Serhii <sergiomakafon@gmail.com>
2022-06-21 12:38:38 +0300

    Function that takes time in minutes and returns hours and minutes (limit 600 minutes)

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 "use strict";
 
-function getCoupeNumber(num) {
-   if (num > 0 && num <= 36 && Number.isInteger(num)) {
-      switch (num) {
-         case 1: case 2: case 3: case 4: return (1);
-         case 5: case 6: case 7: case 8: return (2);
-         case 9: case 10: case 11: case 12: return (3);
-         case 13: case 14: case 15: case 16: return (4);
-         case 17: case 18: case 19: case 20: return (5);
-         case 21: case 22: case 23: case 24: return (6);
-         case 25: case 26: case 27: case 28: return (7);
-         case 29: case 30: case 31: case 32: return (8);
-         case 33: case 34: case 35: case 36: return (9);
+function getTimeFromMinutes(time) {
+   let changedTime;
+   let onlyHours;
+   let onlyMinutes;
+   if (time >= 0 && time < 600 && Number.isInteger(time)) {
+      if(time >= 60 && time < 120){
+         onlyHours = 1;
+      }else{
+         changedTime = time / 60;
+         onlyHours = Math.floor(changedTime);
       }
-   } else if (num == 0 || num > 36) {
-      return ('Таких мест в вагоне не существует');
-   } else {
-      return ('Ошибка.Проверьте правильность введенного момера');
+      onlyMinutes = time - onlyHours * 60;
+      switch (onlyHours) {
+         case 0: case 5: case 6: case 7: case 8: case 9: case 10:  
+         return (`Это ${onlyHours} часов и ${onlyMinutes} минут`);
+         case 2: case 3: case 4:   
+         return (`Это ${onlyHours} часа и ${onlyMinutes} минут`);
+         case 1:   
+         return (`Это ${onlyHours} час и ${onlyMinutes} минут`);   
+      }
+   }else{
+      return ('Ошибка проверьте данные');
    }
 }
-console.log(getCoupeNumber(33));
-console.log(getCoupeNumber(7));
-console.log(getCoupeNumber(300));
-console.log(getCoupeNumber(0));
-console.log(getCoupeNumber(7.7));
-console.log(getCoupeNumber(-10));
-console.log(getCoupeNumber('Hello'));
-
+console.log(getTimeFromMinutes(60));
+console.log(getTimeFromMinutes(200));
+console.log(getTimeFromMinutes(50));
+console.log(getTimeFromMinutes(-150));
