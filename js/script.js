a1ee2695f6a4e03d902ea51b17eee860169964a4
Serhii <sergiomakafon@gmail.com>
2022-06-23 12:27:39 +0300

    A function that checks if the budget for heating is enough

--- js/script.js
+++ js/script.js
@@ -1,31 +1,50 @@
 "use strict";
 
-const someString = 'This is some strange string';
-
-function reverseString(str) {
-   if(typeof(str) !== 'string'){
-      return ('Ошибка!');
-   }else{
-      let arr = [];
-      for (let key in str) {
-         arr[key] = str[key];
+const shoppingMallData = {
+   shops: [
+      {
+         width: 10,
+         length: 5
+      },
+      {
+         width: 15,
+         length: 7
+      },
+      {
+         width: 20,
+         length: 5
+      },
+      {
+         width: 8,
+         length: 10
       }
-      let newArr = arr.slice().reverse();
-      return newArr.join('');
-   }
-}
-function reverseStringQuick(str) {
-   if(typeof(str) !== 'string'){
-      return ('Ошибка!');
-   }else{
-      return str.split('').reverse().join('');
-   }
-}
+   ],
+   height: 5,
+   moneyPer1m3: 30,
+   budget: 50000
+};
 
-console.log(reverseString(someString));
-console.log(reverseString(10));
-console.log(reverseStringQuick(someString));
-console.log(reverseStringQuick(10));
+function isBudgetEnough(data) {
+   let shopsArea = 0;
+   for (let key in data.shops) {
+      let currenResult = data.shops[key].width * data.shops[key].length;
+      shopsArea += currenResult;
+   }
+   let mallArea = shopsArea * data.height;
+   let totalBudget = mallArea * data.moneyPer1m3;
+   if(totalBudget < data.budget){
+      return ('Бюджета достаточно');
+   }else{
+      return ('Бюджета недостаточно');
+   }
+}
+console.log(isBudgetEnough(shoppingMallData)); 
+
+
+
+
+
+
 
 
 
