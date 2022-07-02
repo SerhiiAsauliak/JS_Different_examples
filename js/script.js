873ebf42b36d76f1866c66471f0237ee9ced1948
Serhii <sergiomakafon@gmail.com>
2022-07-03 00:10:40 +0300

    Function that defines that the string is a pangram or not

--- js/script.js
+++ js/script.js
@@ -1,67 +1,19 @@
 "use strict";
 
-let company = {
-  sales: [{
-    name: 'John',
-    salary: 1000
-  }, {
-    name: 'Alice',
-    salary: 600
-  }],
-  development: {
-    sites: [{
-      name: 'Peter',
-      salary: 2000
-    }, {
-      name: 'Alex',
-      salary: 1800
-    }],
-    internals: [{
-      name: 'Jack',
-      salary: 1300
-    }],
-  }
-};
-
-function salarySumByRecursion(data) {
-  if (Array.isArray(data)) {
-    let total = 0;
-    for (let i = 0; i < data.length; i++) {
-      total += data[i].salary;
-    }
-    return [total, data.length];
-  } else {
-    let total = [0, 0];
-     for(let el of Object.values(data)){
-      let dataArray = salarySumByRecursion(el);
-      total[0] += dataArray[0];
-      total[1] += dataArray[1];
-     }
-     return total;
-  }
-}
-console.log(salarySumByRecursion(company));
-
-function salarySum(data) {
-  let personCount = 0;
-  let salarySum = 0;
-  for(let el of Object.values(data)){
-    if(Array.isArray(el)){
-      for (let i = 0; i < el.length; i++) {
-        salarySum  += el[i].salary;
-        personCount++; 
-      }
-    }else{
-      for(let key of Object.values(el)){
-        for (let i = 0; i < key.length; i++) {
-          salarySum  += key[i].salary;
-          personCount++; 
-        }
+  function isPangram(string) {
+    let counter = 0;
+    for(let el of new Set(string.toLowerCase().split(''))){
+      if(el != ' ' && el != ',' && el != '.'){
+        counter++;
       }
     }
+    return counter === 26 ? true : false;
   }
-  return (`All salary: ${salarySum}; Count of workers: ${personCount}`);
-}
-console.log(salarySum(company));
 
+
+console.log('1:' + isPangram('The quick brown fox jumps over the lazy dog')); 
+console.log('2:' + isPangram('The quick brown fox')); 
+console.log('3:' + isPangram('Veldt jynx grimps waqf zho buck')); 
+console.log('4:' + isPangram('Only the fool would take trouble to verify that his sentence was composed of ten as, three bs, four cs')); 
+console.log('6:' + isPangram('I, quartz pyx, who fling muck beds')); 
 
